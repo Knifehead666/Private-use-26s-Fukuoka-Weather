b189b8eb5eafe37ba9a0a4ac1aa25027521a48e7
Knifehead666 <legnakrad326@gmail.com>
2026-09-09 09:23:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fukuoka-weather-v10";
+const CACHE = "fukuoka-weather-v11";
 const SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
